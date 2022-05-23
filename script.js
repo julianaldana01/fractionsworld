3f32aabfa21f6d251f677f9b780b33095ecca311
julianaldana01 <106123624+julianaldana01@users.noreply.github.com>
2022-05-23 18:59:32 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("Hello 🌎");
+
+let popup = document.getElementById("popup");
+
+      function abrirPopup(){
+
+        popup.classList.add("open-popup");
+      }
+      
+      function cerrarPopup(){
+        
+        popup.classList.remove("open-popup");
+        
+      }
+let popup1 = document.getElementById("popup1");
+
+      function abrirPopup1(){
+
+        popup1.classList.add("open-popup1");
+      }
+      
+      function cerrarPopup1(){
+        
+        popup1.classList.remove("open-popup1");
+        
+      }
